89f2668c9b20accbd21951c92151b9ade7c50334
wangfuyun <1585871647@qq.com>
2022-09-05 16:32:36 +0800

    Star over

--- src/components/ShopList/ShopList.vue
+++ src/components/ShopList/ShopList.vue
@@ -17,13 +17,14 @@
             </section>
             <section class="shop_rating_order">
               <section class="shop_rating_order_left">
-                <div class="star star-24">
+<!--                <div class="star star-24">
                   <span class="star-item on"></span>
                   <span class="star-item on"></span>
                   <span class="star-item on"></span>
                   <span class="star-item half"></span>
                   <span class="star-item off"></span>
-                </div>
+                </div>-->
+                <Star :score="shop.rating" :size="24"></Star>
                 <div class="rating_section">
                   {{ shop.rating }}
                 </div>
@@ -56,7 +57,9 @@
 
 <script>
 import {mapState} from 'vuex'
+import Star from '../star/star'
 export default {
+  components: {Star},
   computed:{
     ...mapState(['shops'])
   },
